78a75df86a5bdeab21e6359c08442c607660de4f
Andrei Vacariu <vacariu.v@gmail.com>
2013-07-30 20:34:11 -0700

    Added virtualenv stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
+require 'capistrano-virtualenv'
+
 set :application, "utils"
 set :deploy_to, "/var/www/utils"
 
 set :repository,  "https://github.com/vlad003/utils-site.git"
 set :scm, :git
-set :branch, "master"
+set :branch, "develop"
 
 role :web, "ec2.avacariu.me"                          # Your HTTP server, Apache/etc
 role :app, "ec2.avacariu.me"                          # This may be the same as your `Web` server
@@ -16,6 +18,11 @@ default_run_options[:pty] = true
 set :permission_method, :acl
 set :use_set_permissions, true
 
+# virtualenv
+set :virtualenv_current_path, "venv"
+set :virtualenv_release_path, "venv"
+set :virtualenv_shared_path, "venv"
+
 namespace :deploy do
     task :reload_uwsgi do
         run "touch #{deploy_to}/UWSGI_RELOAD"
@@ -25,6 +32,12 @@ end
 namespace :utils do
     task :run_puppet do
         run "sudo puppet agent -t; true"
+    end
+    task :show_vars do
+        puts "#{current_path}\n"
+        puts "#{release_path}\n"
+        puts "#{shared_path}\n"
+
     end
 end
 
